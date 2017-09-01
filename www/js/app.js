9ae5b80f2555f7445e3b174b09aac3c852fb47f9
Patrick <suckmyjunkmail.lol@gmail.com>
2017-09-01 15:12:43 -0400

    Updated app.js - working APPgit add .!

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ui.router', 'main', 'city'])
-
+	
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
